e4d685de7d1f2c28d6c2ca8cf7a36a8096916dc4
jwl <jwl@cloverwall.net>
2017-05-28 14:12:25 -0700

    Initial commit of code starting from the end of Lesson 22

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBE5lBjxPKgFEDHxO3wTAHMnttpkW2Vzrc",
+  authDomain: "catch-of-the-day-jwl.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-jwl.firebaseio.com",
+});
+
+export default base;
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <h2>Not Found!11!!</h2>
+    )
+  }
+}
+
+export default NotFound;
+
+
