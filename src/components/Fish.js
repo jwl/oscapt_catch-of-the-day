c4c9fa1ea3ec7efbf97a4e09863f5f8def899703
jwl <jwl@cloverwall.net>
2017-06-03 15:32:37 -0700

    Updated to end of Lesson 23.

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -21,7 +21,9 @@ class Fish extends React.Component {
 }
 
 Fish.propTypes = {
+  details: React.PropTypes.object.isRequired,
+  index: React.PropTypes.string.isRequired,
   addToOrder: React.PropTypes.func.isRequired
-}
+};
 
 export default Fish;
